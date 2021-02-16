5b550710cd4ae99c783e073e5751c0fe1f17beb7
Jeremias Erbs <jerry@badabam.de>
2021-02-16 15:47:13 +0100

    Add details logic for Card

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import { results } from './rickandmortyapi.json'
 function App() {
   return (
     <div className="App">
-      {results.map(({ name, species, image, id }) => (
-        <Card key={id} name={name} species={species} image={image} />
-      ))}
+      {results.map(
+        ({ name, species, image, id, gender, status, origin, location }) => (
+          <Card
+            key={id}
+            name={name}
+            species={species}
+            gender={gender}
+            image={image}
+            status={status}
+            origin={origin.name}
+            location={location.name}
+          />
+        )
+      )}
     </div>
   )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,17 @@
+import { useState } from 'react'
 import './Card.css'
 
-export default function Card({ name, species, image }) {
+export default function Card({
+  name,
+  species,
+  image,
+  status,
+  origin,
+  location,
+  gender,
+}) {
   const icon = species === 'Human' ? '👤' : '👽'
+  const [areDetailsVisible, setAreDetailsVisible] = useState(false)
 
   return (
     <section className="Card">
@@ -9,6 +19,19 @@ export default function Card({ name, species, image }) {
         {icon} {name}
       </h2>
       <img src={image} alt="" />
+      <button onClick={() => setAreDetailsVisible(!areDetailsVisible)}>
+        {areDetailsVisible ? 'Hide details' : 'Show details'}
+      </button>
+      <dl hidden={!areDetailsVisible}>
+        <dt>Gender</dt>
+        <dd>{gender}</dd>
+        <dt>Status</dt>
+        <dd>{status}</dd>
+        <dt>Origin</dt>
+        <dd>{origin}</dd>
+        <dt>Location</dt>
+        <dd>{location}</dd>
+      </dl>
     </section>
   )
 }
